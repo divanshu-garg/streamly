4460702b8dee22d3690b16184dda14a876b7e1e4
Divanshu Garg <divanshugarg49@gmail.com>
2024-05-29 20:40:37 +0530

    added some util files for streamlining code

--- src/index.js
+++ src/index.js
@@ -5,7 +5,20 @@ import { DB_NAME } from "./constants.js";
 
 import connectDB from "./db/index.js";
 
-connectDB();
+connectDB()
+.then(() => {
+
+    app.on("error", () => {
+        console.log("Error faced: ", error);
+        throw error;
+    })
+    app.listen(process.env.PORT || 8000, () => {
+        console.log("Server is running at port: ", process.env.PORT);
+    })
+})
+.catch((error) => {
+    console.log("MongoDB connection failed: ", error);
+})
 
 
 
