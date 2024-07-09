24ca602aaedf3c68cb4be844a699881ba735a5e6
Divanshu Garg <divanshugarg49@gmail.com>
2024-07-09 14:03:00 +0530

    added remaining user controller routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,32 +1,57 @@
 import { Router } from "express";
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
-import {upload} from "../middlewares/multer.middleware.js"
+import {
+  changeCurrentPassword,
+  getCurrentUser,
+  loginUser,
+  logoutUser,
+  refreshAccessToken,
+  registerUser,
+  updateUserAvatar,
+  updateUserCoverImage,
+} from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.route("/register").post(
-    upload.fields([
-        {
-            name: "avatar",
-            maxCount: 1 
-        },
-        // console.log("log1"),
-        {
-            name: "coverImage",
-            maxCount: 1
-        }
-    ])
-    ,registerUser
-)
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    // console.log("log1"),
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
+);
 
-router.route("/login").post(loginUser)
+router.route("/login").post(loginUser);
 
-router.route("/logout").post(
-    verifyJWT,
-    logoutUser)
+router.route("/logout").post(verifyJWT, logoutUser);
 // logout: middlewares extracts user from token and adds it to req.body, controller actually deletes token from user object
 
-router.route("/refresh-tokens").post(refreshAccessToken)
+router.route("/refresh-tokens").post(refreshAccessToken);
 
-export default router
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-details").patch(verifyJWT, updateAccountDetails);
+router
+  .route("/change-avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+
+router
+  .route("/change-cover-image")
+  .patch(
+    verifyJWT,
+    upload.single("coverImage"),
+    updateUserCoverImage
+  );
+
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+router.route("/watch-history").get(verifyJWT, getWatchHistory)
+
+export default router;
