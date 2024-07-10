802f9550cdfa42d357527619f83427d89636b00a
Divanshu Garg <divanshugarg49@gmail.com>
2024-07-10 18:35:08 +0530

    added get all subscribers and get subscribed channels controllers

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -1,6 +1,7 @@
 import asyncHandler from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/apiError.js";
 import { Subscription } from "../models/subscription.model.js";
+import { User } from "../models/user.model.js"
 import mongoose from "mongoose";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
@@ -50,4 +51,63 @@ const toggleSubscription = asyncHandler(async (req, res) => {
   }
 });
 
-export { toggleSubscription };
+const getUserChannelSubscribers = asyncHandler(async(req, res) => {
+    // error handling on channelId
+    // fetch subscribers array, error handling
+    // return subscribers array
+
+
+    const { channelId } = req.params
+
+    const channel = await User.findById(channelId)
+
+    if(!channel){
+        throw new ApiError(404, "channel not found", error?.message)
+    }
+
+    const subscribers = await Subscription.find({ channel: channelId })
+
+    if(!subscribers){
+        throw new ApiError(500, "an error occured while fetching subscribers", error?.message)
+    }
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, {
+        subscribers: subscribers.length
+    }, 
+    "subscribers fetched successfully"
+))
+    
+})
+
+const getSubscribedChannels = asyncHandler(async(req, res) => {
+
+    const { subscriberId } = req.params
+
+    const subscriber = await User.findById(subscriberId)
+
+    if(!subscriber){
+        throw new ApiError(404, "user not found", error?.message)
+    }
+
+    const subscribedChannels = await Subscription.find({ subscriber: subscriberId })
+
+    if(!subscribedChannels){
+        throw new ApiError(500, "something went wrong while finding subscribers", error?.message)
+    }
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, {
+        subscribedChannels: subscribedChannels
+    }))
+
+})
+
+
+export { 
+    toggleSubscription,
+    getUserChannelSubscribers,
+    getSubscribedChannels
+ };
